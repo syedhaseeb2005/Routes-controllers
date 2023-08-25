9cddc39871f9b23e1f84c7f9afe1f6cd2f08f61e
syedhaseeb2005 <haseebsyed444@gmail.com>
2023-08-25 19:24:14 +0500

    Mongo Connection

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 import express from 'express'
 import authrouth from './Routes/authRouth.js'
 import profileRouth from './Routes/ProfileRouth.js'
+import mongoose from 'mongoose'
 
-
+let Mongo_Url = 'mongodb+srv://syedhaseeb123:syedhaseeb1@mymongodb.m7inx8j.mongodb.net/'
 
 const app = express()
 
 const port = 8000;
 
 app.use(express.json())
+
+const connectDB = ()=>{
+    mongoose.connect(Mongo_Url)
+    .then(()=>{
+        console.log('Connected data');
+    }).catch((err)=>{
+        throw err;
+    })
+}
+
+
+
+
 
 
 app.use((req,res,next)=>{
@@ -23,5 +37,6 @@ app.use('/auth',authrouth)
 app.use('/profile',profileRouth)
 
 app.listen(port,()=>{
+    connectDB()
     console.log(`server chal raha hy at port number ${port}`);
 })
